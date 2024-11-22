e9ecb66bf27deff95c16ff82254905e179be4ae5
saadmbt <saadnh05@gmail.com>
2024-11-22 18:24:57 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Sql_Ai_query_generator/',
+  base: '/Sql_Ai_query_generator_FrontEnd/',
 })
